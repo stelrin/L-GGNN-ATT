abcf5502c5aae0a529dfc055eba3bc8added10c1
stelrin <gaf.moali@gmail.com>
2022-12-13 14:54:40 +0100

    Refactor variable names

--- preprocessing/prime_dataset.py
+++ preprocessing/prime_dataset.py
@@ -9,14 +9,14 @@ def get_dataset_metadata(dataset_directory: str):
     train_dataset_size = 0
     test_dataset_size = 0
 
-    for type in ["train", "test"]:
-        with open(f"datasets/{dataset_directory}/{type}.csv", "r") as data_file:
+    for dataset_type in ["train", "test"]:
+        with open(f"datasets/{dataset_directory}/{dataset_type}.csv", "r") as data_file:
             data = [list(map(int, rec)) for rec in csv.reader(data_file, delimiter=",")]
             item_count = max(item_count, np.amax([np.amax(z) for z in data]) + 1)  # (+ 1) for that extra 0 as the item ids start at 1
             max_sequence_len = max(max_sequence_len, len(max(data, key=len)))
             max_number_of_nodes = max(max_number_of_nodes, len(max([np.unique(i) for i in data], key=len)))
 
-            if type == "train":
+            if dataset_type == "train":
                 train_dataset_size = len(data)
             else:
                 test_dataset_size = len(data)
