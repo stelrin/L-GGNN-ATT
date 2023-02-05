661ed8d5a098a79216666de2c538918b22c2c9f4
stelrin <gaf.moali@gmail.com>
2023-02-05 17:41:52 +0100

    Add lossless graph formulation

--- preprocessing/graph_formulation.py
+++ preprocessing/graph_formulation.py
@@ -31,7 +31,7 @@ def graph_formulation(row):
         tf.stack([tf.range(vector_length - 1), tf.range(vector_length - 1) + 1], axis=0),
     )  # Stack and stagger values
 
-    # TODO: Change graph formulation here
+    # Grabbing the unique indices
     unique_indices, _ = tf.unique(indices[0] * (vector_length + 1) + indices[1])  # unique(a*x + b)
 
     unique_indices = tf.sort(unique_indices)  # Sort ascendingly
@@ -53,12 +53,12 @@ def graph_formulation(row):
     adj = tf.sparse.to_dense(adj)
 
     # formulating incoming adjacency matrix
-    u_sum_in_tf = tf.math.reduce_sum(adj, 0)
+    u_sum_in_tf = tf.math.reduce_sum(adj, 0) # Calculates the incoming edges of each node
     u_sum_in_tf = tf.clip_by_value(u_sum_in_tf, 1, tf.reduce_max(u_sum_in_tf))
     adj_in = tf.math.divide(adj, u_sum_in_tf)
 
     # formulating outgoing adjacency matrix
-    u_sum_out_tf = tf.math.reduce_sum(adj, 1)
+    u_sum_out_tf = tf.math.reduce_sum(adj, 1) # Calculates the outgoing edges of each node
     u_sum_out_tf = tf.clip_by_value(u_sum_out_tf, 1, tf.reduce_max(u_sum_out_tf))
     adj_out = tf.math.divide(tf.transpose(adj), u_sum_out_tf)
 
@@ -68,7 +68,44 @@ def graph_formulation(row):
     return adj_in, adj_out, sequence_of_indexes, session_items, mask, target
 
 
-# TODO: Move this to prime_dataset.py
+def graph_formulation_lossless(row):
+    features = row[:-1]
+    target = row[-1]
+    session_items, sequence_of_indexes = tf.unique(features)
+    mask = tf.fill(tf.shape(features), 1)
+
+    vector_length = tf.shape(features)[0]
+    n_nodes = tf.shape(session_items)[0]
+    order = [0.0 for _ in range(n_nodes)]
+    indices = []
+    values = []
+
+    for i in range(vector_length - 1):
+        order[sequence_of_indexes[i + 1]] += 1.0
+        indices.append([sequence_of_indexes[i].numpy(), sequence_of_indexes[i + 1].numpy()])
+        values.append(order[sequence_of_indexes[i + 1].numpy()])
+
+    indices = tf.constant(indices, dtype=tf.int64)
+    values = tf.constant(values, dtype=tf.float32)
+    
+    max_weight = tf.reduce_max(values)
+
+    sparse_adjacency = tf.sparse.SparseTensor(dense_shape=[n_nodes, n_nodes], values=values, indices=indices)
+    sparse_adjacency = tf.sparse.reorder(sparse_adjacency)
+    
+    adj_in = tf.sparse.to_dense(sparse_adjacency)
+    
+    # Normalizing the adjacency matrices
+    adj_in = tf.divide(adj_in, max_weight)
+    adj_out = tf.transpose(adj_in)
+    
+    # The adjacency matrices appear to be transposed in the original implementation;
+    # This explains some of the unsimilarities between the implementation and the original paper.
+    return adj_in, adj_out, sequence_of_indexes, session_items, mask, target
+
+
+# TODO: Move this to prime_dataset.py and add train_dataset_size and test_dataset_size to the metadata
+# prime_dataset.py/get_dataset_metadata should return a DatasetMetadata object (to get rid of the mess in main.py)
 class DatasetMetadata(NamedTuple):
     name: str
     max_number_of_nodes: int
@@ -82,6 +119,7 @@ def get_dataset(dataset_info: DatasetMetadata, batch_size: int, train: bool = Tr
 
     dataset = tf.data.Dataset.from_generator(partial(generator, data), output_types=(tf.int32))
 
+    # FIXME: this is redundant, formulate the graphs directly in the generator!
     dataset = dataset.map(graph_formulation, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
     # TODO: shuffle should take the size of the train/test dataset
